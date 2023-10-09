faa2b35fe86dce35ba628f409b2fc20470fb2be4
Santiago Crippa <santicrippa@gmail.com>
2023-10-09 19:20:28 -0300

    calling supabase data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-const Home = () => {
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+const Home = async () => {
+  const supabase = createServerComponentClient({ cookies })
+  const { data: posts } = await supabase.from('posts').select('*')
+
   return (
     <>
-      <h1>Welcome To X clone!</h1>
+      <div className='container w-full text-center'>
+        <h1>Welcome To X clone!</h1>
+        <pre>
+          {
+            JSON.stringify(posts, null, 2)
+          }
+        </pre>
+      </div>
     </>
   )
 }
