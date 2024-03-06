2c1c2625ddd13eabaa51749248ae3bb22012c3e5
Santiago Crippa <santicrippa@gmail.com>
2024-03-06 17:00:44 -0300

    reconfiguring structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import AuthBtn from '@/components/common/auth-btn'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
+import AuthBtn from '@/components/common/auth-btn'
+
 const Home = async () => {
-  const supabase = createServerComponentClient({ cookies })
+  const supabase = createServerComponentClient({ cookies }) // como es un componente que esta del lado del servidor, podemos acceder a datos como las cookies. Y necesitamos las cookies para que supabase sepa si el user esta logeado y demas cosas.
   const { data: posts } = await supabase.from('posts').select('*')
 
   return (
@@ -11,7 +12,7 @@ const Home = async () => {
       <div className='flex min-h-screen flex-col items-center justify-center p-24'>
         <h1>Welcome To X clone!</h1>
         <AuthBtn />
-        <main className=''>
+        <main>
           {
             JSON.stringify(posts, null, 2)
           }
