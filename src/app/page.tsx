88b0af864ccb3a385179d7f6ae66bf136f25aac5
Santiago Crippa <santicrippa@gmail.com>
2024-03-11 17:54:20 -0300

    adding posts cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 import { AuthBtnServer } from '@/components/common/auth-btn-server'
+import PostCard from '@/components/post-card'
 
 export default async function Home () {
   // como es un server-component, podemos acceder a datos como las cookies u las necesitamos para que supabase sepa si el user esta logeado, entre otras cosas.
@@ -16,18 +17,38 @@ export default async function Home () {
 
   const { data: posts } = await supabase
     .from('posts')
-    .select('*, users(name, avatar_url, user_name)')
+    .select('*, user:users(name, avatar_url, user_name)')
 
   return (
     <>
       <main className='flex min-h-screen flex-col items-center justify-center p-24'>
         <h2 className='text-xl font-bold mb-4'>Welcome to X clone!</h2>
         <AuthBtnServer />
-        <pre className='mt-10'>
-          {
-            JSON.stringify(posts, null, 2)
-          }
-        </pre>
+        {
+          posts?.map(post => {
+            const {
+              id,
+              user,
+              content
+            } = post
+
+            const {
+              user_name: userName,
+              name: userFullName,
+              avatar_url: avatarUrl
+            } = user
+
+            return (
+              <PostCard
+                key={id}
+                userName={userName}
+                userFullName={userFullName}
+                avatarUrl={avatarUrl}
+                content={content}
+              />
+            )
+          })
+        }
       </main>
     </>
   )
---
+++ src/components/post-card.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import Link from 'next/link'
+import { Card, CardHeader, CardBody, CardFooter, Avatar } from '@nextui-org/react'
+
+export default function PostCard ({
+  userFullName,
+  userName,
+  avatarUrl,
+  content
+}: {
+  userFullName: string
+  userName: string
+  avatarUrl: string
+  content: string
+}) {
+  return (
+    <Card className="max-w-[340px]">
+      <CardHeader className="justify-between">
+        <div className="flex gap-x-2">
+          <Link href={`/${userName}`}>
+            <Avatar radius="full" size="md" src={avatarUrl} />
+          </Link>
+          <div className="flex flex-col gap-1 items-start justify-center">
+            <h4 className="text-small font-semibold leading-none text-default-600">{userFullName}</h4>
+            <h5 className="text-small tracking-tight text-default-400">@{userName}</h5>
+          </div>
+        </div>
+      </CardHeader>
+      <CardBody className="px-3 py-0 text-small text-white">
+        <p>
+          {content}
+        </p>
+      </CardBody>
+      <CardFooter className="gap-3">
+        <div className="flex gap-1">
+          <p className="font-semibold text-default-400 text-small">4</p>
+          <p className=" text-default-400 text-small">Following</p>
+        </div>
+        <div className="flex gap-1">
+          <p className="font-semibold text-default-400 text-small">97.1K</p>
+          <p className="text-default-400 text-small">Followers</p>
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
